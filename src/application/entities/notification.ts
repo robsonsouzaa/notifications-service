a0a05dbdc9f8369ce4c1c2797e30eb542efa053c
Robson Souza <robsonsouzaprog@gmail.com>
2022-12-16 10:05:23 -0400

    feat: adiciona testes de notificação e conteúdo

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,58 @@
+import { Replace } from 'src/helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+
+  recipientId: string;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+}
